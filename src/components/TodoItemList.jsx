07e42e659434dd293b0a8498bc758ddad0de9abd
TP9NS <pshccxx@gmail.com>
2025-05-23 16:54:46 +0900

    step2

--- src/components/TodoItemList.jsx
+++ src/components/TodoItemList.jsx
@@ -1,28 +1,51 @@
-import { Component } from "react";
-import TodoItem from "./TodoItem";
-import PropTypes from "prop-types";
+import { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux'
+
+import TodoItem from '@components/TodoItem';
+import { fetchAllTodos } from '@/actions'
+
 class TodoItemList extends Component {
-  shouldComponentUpdate(nextProps, nextState) {
-    return this.props.myTodos !== nextProps.myTodos;
-  }
-  render() {
-    const { myTodos, myToggle, myRemove } = this.props;
-    const todoList = myTodos.map(({ id, text, checked }) => (
-      <TodoItem
-        id={id}
-        text={text}
-        checked={checked}
-        onToggle={myToggle}
-        onRemove={myRemove}
-        key={id}
-      />
-    ));
-    return <div>{todoList}</div>;
-  }
+    /*
+        true(myTodos 변수에 변동이 있으면) 이면 render() 함수가 호출됨
+        false(myTodos 변수에 변동이 없으면) 이면 render() 함수가 호출되지 않음 (렌더링 생략)
+    */
+    shouldComponentUpdate(nextProps, nextState) {
+        return this.props.myTodos !== nextProps.myTodos;
+    }
+    //HTML DOM 렌더링 후에 호출되는 lifecyle method
+    componentDidMount() {
+        this.props.fetchAll();
+    }
+
+    render() {
+        const { myTodos } = this.props;
+        const todoList = myTodos.map(
+            ({ id, text, checked }) => (
+                <TodoItem
+                    id={id}
+                    text={text}
+                    checked={checked}
+                    key={id}
+                />
+            )
+        );
+        return (
+            <div>
+                {todoList}
+            </div>
+        );
+    }
 }
+
 TodoItemList.propTypes = {
-  myTodos: PropTypes.array,
-  myToggle: PropTypes.func,
-  myRemove: PropTypes.func,
+    myTodos: PropTypes.array,
+    fetchAll : PropTypes.func
 };
-export default TodoItemList;
+
+export default connect(
+    //store에 저장된 state 객체의 todos 를 가져와서 myTodos 이름에 매핑
+    (state) => ({myTodos:state.todos}), 
+    //action함수를 dispatch 하는 함수를 fetchAll 이름에 매핑
+    { fetchAll: fetchAllTodos }
+)(TodoItemList);
--- src/components/TodoListTemplate.jsx
+++ src/components/TodoListTemplate.jsx
@@ -1,10 +1,10 @@
-import './TodoListTemplate.css';
+import '@components/TodoListTemplate.css';
 import PropTypes from 'prop-types';
 const TodoListTemplate = ({ form, children }) => {
     return (
         <main className="todo-list-template">
             <div className="title">
-                오늘의 할 일
+                오늘의 할 일 고양이는 지은({import.meta.env.VITE_MODE})
             </div>
             <section className="form-wrapper">
                 {form}
